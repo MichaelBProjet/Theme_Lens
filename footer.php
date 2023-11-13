ff22bb2b0354d3dd1dd09e96ba4b41a510131868
A699203 <michael.boujlifa@atos.net>
2023-11-13 15:58:22 +0100

    passage en htokml

--- footer.php
+++
@@ -1,8 +0,0 @@
-<a class="survoltp" href="index.php">
-            <img src = "images/logo.png" alt="Logo Robbie Lens"/><br>
-</a>
-<div>
-    <a class="survoltp" href="https://www.tiktok.com/fr/" target ="_blank">
-        <img style="max-height: 40px;" src = "TikTok_logo.svg" alt="logo TikTok"/>
-    </a>
-</div>
